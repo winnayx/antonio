0212144f9485ba693103eb72e23cc59ec1927125
Winnie Xiao <winniex@stanford.edu>
2020-12-18 11:16:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+import { DatePicker, InputNumber, Card } from "antd";
+
+const { RangePicker } = DatePicker;
+const tabList = [
+  {
+    key: "tab1",
+    tab: "Hello",
+  },
+  {
+    key: "tab2",
+    tab: "Goodbye",
+  },
+];
+
+const contentList = {
+  tab1: <p>content1</p>,
+  tab2: <p>content2</p>,
+};
+
+function Elephant() {
+  const [key, setKey] = useState("tab1");
+  function onTabChange(key, type) {
+    console.log(key, type);
+    setKey(key);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>example</h1>
+      <p>how much would you pay?</p>
+      <InputNumber />
+      <p>
+        <a href="https://socialmagicweekly.substack.com/">subscribe!</a>
+      </p>
+      <Card
+        style={{ width: "50%", textAlign: "left" }}
+        title="Card title"
+        extra={<a href="#">More</a>}
+        tabList={tabList}
+        activeTabKey={key}
+        onTabChange={(key) => {
+          onTabChange(key, "key");
+        }}
+      >
+        {contentList[key]}
+      </Card>
+      <RangePicker style={{ margin: "10px" }} />
     </div>
   );
 }
 
-export default App;
+export default Elephant;
